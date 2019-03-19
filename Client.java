0b95499b481a1f4bcfb8fe6de09c07225506570c
hakimnorizman <30333599+hakimnorizman@users.noreply.github.com>
2019-03-20 00:21:23 +0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -4,22 +4,24 @@ class Client
 {
   public static void main(String[] args) throws Exception
   {
-     Socket sock = new Socket("192.168.242.128", 6666);
-                               // reading from keyboard (keyRead object)
-     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-                              // sending to client (pwrite object)
+     Socket sock = new Socket("192.168.242.128", 6666); // reading from keyboard (keyRead object)
+    
+     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in)); // sending to client (pwrite object)
+    
      OutputStream ostream = sock.getOutputStream();
-     PrintWriter pwrite = new PrintWriter(ostream, true);
-
-                              // receiving from server ( receiveRead  object)
+    
+     PrintWriter pwrite = new PrintWriter(ostream, true); // receiving from server ( receiveRead  object)
+     
      InputStream istream = sock.getInputStream();
+     
      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
      System.out.println("Begin Conversation:");
 
      String receiveMessage, sendMessage;
+     
      while(true)
-     {
+      {
         sendMessage = keyRead.readLine();  // keyboard reading
         pwrite.println(sendMessage);       // sending to server
         pwrite.flush();                    // flush the data
@@ -29,4 +31,4 @@ class Client
         }
       }
     }
-}
+ }
